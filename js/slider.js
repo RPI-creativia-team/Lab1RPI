b6518a2893cd892b8b1b0a99f6712df2a0dd3e1d
Roman Shidlovsky <romanshidlovsky@mail.ru>
2022-05-12 14:49:47 +0300

    add slider

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,13 @@
-function createSlider(num, parent_id, text, img)
+function createSlider(num, parent_id, text, img, comment)
 {
+    console.log(num)
     const parent = document.getElementById(parent_id)
+
+    const comm = document.createElement('p')
+    comm.id = 'comm'
+    comm.textContent = comment
+    comm.className = 'n-5page-data-text'
+
     const imgDiv = document.createElement('div')
     imgDiv.id = "img-div"
 
@@ -34,11 +41,12 @@ function createSlider(num, parent_id, text, img)
         selectorDiv.onclick = function () {
             localStorage.setItem('num',JSON.stringify(i))
             removeSlider('n-5page-data')
-            createSlider(i,'n-5page-data',names[i],pics[i])
+            createSlider(i,'n-5page-data',names[i],pics[i],comments[i])
         }
         contDiv.appendChild(selectorDiv)
     }
 
+    parent.appendChild(comm)
     parent.appendChild(imgDiv)
     parent.appendChild(contDiv)
 }
@@ -47,26 +55,40 @@ function removeSlider(parent_id)
 {
     const parent = document.getElementById(parent_id)
 
+    const comm = document.getElementById('comm')
     const imgDiv = document.getElementById('img-div')
     const contDiv = document.getElementById('cont-div')
 
+    parent.removeChild(comm)
     parent.removeChild(imgDiv)
     parent.removeChild(contDiv)
 }
 
 const names = ['AMR SROUR','NIKITA HRIPACH','ROMAN SHIDLOVSKY']
 const pics = ['images/nImg/pg5-man-hex.png','images/nImg/hripach.png','images/nImg/shidlovsky.png']
+const comments = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed magna vel' +
+' velit dignissim luctus eu in urna. Dapibus egestas turpis.',
+    'At vero eos et accusamus et iusto odio ' +
+'dignissimos ducimus qui blanditiis praesentium. ' +
+' Et harum quidem rerum facilis est et expedita distinctio.',
+    'On the other hand, we denounce with righteous' +
+' indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment.']
 
 function setSlider()
 {
 
     let num = JSON.parse(localStorage.getItem('num'))
     num = (num + 1) % 3
-    removeSlider('n-5page-data')
-    createSlider(num,'n-5page-data',names[num],pics[num])
+    try {
+        removeSlider('n-5page-data')
+    }catch{
+        console.log('error')
+    }
+
+    createSlider(num,'n-5page-data',names[num],pics[num],comments[num])
     localStorage.setItem('num', JSON.stringify(num))
 }
 const num = JSON.parse(localStorage.getItem('num'))
-createSlider(num,'n-5page-data',names[num],pics[num])
+createSlider(num,'n-5page-data',names[num],pics[num],comments[num])
 
 setInterval(function (){setSlider()}, 7000)
